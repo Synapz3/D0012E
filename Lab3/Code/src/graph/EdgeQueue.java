f2bd6f50e9729bc4c0098827f77c2c9456338e2e
ironedde <edvin.akerfeldt@hotmail.com>
2016-02-02 00:28:29 +0100

    Started implementing d-array as defined on wikipedia

--- Lab3/Code/src/graph/EdgeQueue.java
+++ Lab3/Code/src/graph/EdgeQueue.java
@@ -8,19 +8,71 @@ public class EdgeQueue {
     private int d;
     private Kant[] kants;
     private int size;
-    private int fild;
-
-    public EdgeQueue(int d){
+    private int elements = 0;
+    private int nextFree = 0;
+    private int lastAdded = -1;
+    private ArrayList<Integer> inCurrentLayer;
+    private int shouldBeInLayer = 0;
+    private int numLayers = 1;
+    public EdgeQueue(int d,int maxSize){
         this.d = d;
-        size = 10;
+        size = maxSize;
         kants = new Kant[size];
-        fild = 0;
+        inCurrentLayer = new ArrayList<Integer>();
+        inCurrentLayer.add(0);
     }
 
 
     public void insert(Edge edge, int cost) {
-      //sort queue
+        //sort queue
+        Kant toAdd = new Kant(edge, cost);
+        kants[nextFree] = toAdd;
+        ensureSmallest(nextFree);
+        if(nextFree == 0){
+            inCurrentLayer.clear();
+            nextFree = 1;
+            inCurrentLayer.add(nextFree);
+            shouldBeInLayer = (int)Math.pow(d,numLayers);
+        }
+        else {
+            if (isParentFull(nextFree)) {
 
+                int granParentPos = getParent(getParent(nextFree));
+                int checkForFree = granParentPos;
+
+                while (checkForFree >= 0) {
+                    for(int i=0;i<inCurrentLayer.size();i += d){
+                        if(!isParentFull(inCurrentLayer.get(i)) ){
+
+                        }
+                    }
+                    //If you should start a new layer
+                    int[] parentsChildren = getChildren(getParent(nextFree));
+                    for(int pos:parentsChildren){
+                        if(kants[pos] == null){
+
+                        }
+                    }
+                }
+
+
+                for (int i = 1; i <= d; i++) {
+                    if (kants[granParentPos + i] == null) {
+                        nextFree = granParentPos + i;
+                        return;
+                    }
+                }
+                //If we should start a new layer
+                nextFree = (getParent(nextFree) + d + 1);
+            } else {
+                nextFree += (d + 1);
+            }
+        }
+
+        //if()
+        //kants[pos] = toAdd;
+
+        /*
         if (fild+1 == size) kants = increse();
 
         fild++;
@@ -41,10 +93,44 @@ public class EdgeQueue {
            } else {
                return;
            }
-       }
+       }*/
 
     }
-
+    private boolean isParentFull(int pos){
+        int parentPos = getParent(pos);
+        for(int i=1; i<=d;i++){
+            if(kants[parentPos+i] == null){
+                return false;
+            }
+        }
+        return true;
+    }
+    private void ensureSmallest(int pos){
+        int where = pos;
+        int parentPos;
+        while(where >= 0) {
+            parentPos = getParent(where);
+            if(kants[parentPos].totCost() > kants[where].totCost()) {
+                Kant parent = kants[parentPos];
+                kants[parentPos] = kants[where];
+                kants[where] = parent;
+                where -= (d+1);
+            }
+            else{
+                return;
+            }
+        }
+    }
+    private int[] getChildren(int pos){
+        int[] tmp = new int[d];
+        for(int i = 1;i<=d;i++){
+            tmp[i-1] = (d*pos)+i;
+        }
+        return tmp;
+    }
+    private int getParent(int pos){
+        return (int)Math.floor((pos-1)/d);
+    }
     public Kant pop () {
         Kant tmp = kants[1];
 
